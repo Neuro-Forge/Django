e9848a15c06f3206ff70226fa6a2fdf3d63d4ce5
cozmek ai <cozmekaifacilitatora@gmail.com>
2026-04-27 16:35:37 +0530

    header

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+import '../css/header.css'
+export default function Header(){
+
+    return(
+        <div className="header">
+            <h1>login</h1>
+            <h1>signup</h1>
+            <h1>foods</h1>
+
+        </div>
+    )
+}
